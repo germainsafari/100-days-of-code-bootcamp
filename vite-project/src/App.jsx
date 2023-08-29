25c4590fbb387c130b1456bc2fe3eab30a955659
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-08-29 18:06:25 +0200

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,62 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    setTodos( (currentTodos) => {
+        return [
+          [...todos, 
+            {id:crypto.randomUUID(), title: newItem, completed:false},
+        ]
+    ]})
+      
+  }
+  setNewItem("")
+ 
+function toggleTodo(id, completed) {
+  setTodos(currentTodos => {
+    return currentTodos.map(todo => {
+      if (todo.id == id ) {
+        return {...todo, completed }
+      }
+      return todo
+    })
+  })
+
+}
+
+  return (
+    <>
+  <form onSubmit={handleSubmit} className="new-item-form">
+    <div className="row">
+      <label htmlFor="item"> New item</label>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
+    </div>
+  
+    <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      {todos.map(todo => {
+        return (
+        <li key={todo.id}>
+        <label htmlFor="">
+          <input type="checkbox" checked={todo.completed} onChange={e =>toggleTodo(todo.id, e.target.checked) } />
+          {todo.title}
+        </label>
+        <button className="btn.btn-danger">remove</button>
+      </li>
+      )
+      })}
+      
+      
+    </ul>
+ 
+ 
+  </>
+  )
+}
